52f724742dc108122d36dcc6b15cb01455949ea8
Tim <timwolf1@gmail.com>
2016-02-21 02:17:43 -0500

    First Version

---
+++ Program File Interpreter/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Program_File_Interpreter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.preParse = new System.Windows.Forms.RichTextBox();
+            this.loadthebitch = new System.Windows.Forms.Button();
+            this.postParse = new System.Windows.Forms.RichTextBox();
+            this.fuckclinton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // preParse
+            // 
+            this.preParse.Dock = System.Windows.Forms.DockStyle.Top;
+            this.preParse.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.preParse.Location = new System.Drawing.Point(0, 0);
+            this.preParse.MaximumSize = new System.Drawing.Size(887, 292);
+            this.preParse.Name = "preParse";
+            this.preParse.Size = new System.Drawing.Size(887, 292);
+            this.preParse.TabIndex = 0;
+            this.preParse.Text = resources.GetString("preParse.Text");
+            // 
+            // loadthebitch
+            // 
+            this.loadthebitch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.loadthebitch.Location = new System.Drawing.Point(0, 292);
+            this.loadthebitch.Margin = new System.Windows.Forms.Padding(0, 0, 3, 3);
+            this.loadthebitch.MaximumSize = new System.Drawing.Size(887, 30);
+            this.loadthebitch.Name = "loadthebitch";
+            this.loadthebitch.Size = new System.Drawing.Size(887, 30);
+            this.loadthebitch.TabIndex = 1;
+            this.loadthebitch.Text = "Button which will prepare to fuck clinton";
+            this.loadthebitch.UseVisualStyleBackColor = true;
+            this.loadthebitch.Click += new System.EventHandler(this.loadthebitch_Click);
+            // 
+            // postParse
+            // 
+            this.postParse.Dock = System.Windows.Forms.DockStyle.Top;
+            this.postParse.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postParse.Location = new System.Drawing.Point(0, 352);
+            this.postParse.MinimumSize = new System.Drawing.Size(887, 292);
+            this.postParse.Name = "postParse";
+            this.postParse.Size = new System.Drawing.Size(887, 292);
+            this.postParse.TabIndex = 2;
+            this.postParse.Text = "";
+            // 
+            // fuckclinton
+            // 
+            this.fuckclinton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.fuckclinton.Location = new System.Drawing.Point(0, 322);
+            this.fuckclinton.Margin = new System.Windows.Forms.Padding(0, 0, 3, 3);
+            this.fuckclinton.MaximumSize = new System.Drawing.Size(887, 30);
+            this.fuckclinton.Name = "fuckclinton";
+            this.fuckclinton.Size = new System.Drawing.Size(887, 30);
+            this.fuckclinton.TabIndex = 3;
+            this.fuckclinton.Text = "Fuck Clinton";
+            this.fuckclinton.UseVisualStyleBackColor = true;
+            this.fuckclinton.Click += new System.EventHandler(this.fuckclinton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(887, 644);
+            this.Controls.Add(this.postParse);
+            this.Controls.Add(this.fuckclinton);
+            this.Controls.Add(this.loadthebitch);
+            this.Controls.Add(this.preParse);
+            this.MinimumSize = new System.Drawing.Size(50, 50);
+            this.Name = "Form1";
+            this.Text = "FUCK CLINTON ISAAC FLOWERS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox preParse;
+        private System.Windows.Forms.Button loadthebitch;
+        private System.Windows.Forms.RichTextBox postParse;
+        private System.Windows.Forms.Button fuckclinton;
+    }
+}
+
---
+++ Program File Interpreter/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Program_File_Interpreter
+{
+    public partial class Form1 : Form
+    {
+
+        public List<string> operations = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadthebitch_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text Files (.txt)|*.txt";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Multiselect = false;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.Stream fileStream = openFileDialog1.OpenFile();
+
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+                {
+                    while (!reader.EndOfStream) { 
+                        preParse.AppendText(reader.ReadLine() + Environment.NewLine);
+                    }
+                    removeLastLine(preParse); //removes last line
+                }
+            }
+        }
+
+        private void fuckclinton_Click(object sender, EventArgs e)
+        {
+            parseInitialInput();
+            //translateToBinary();
+        }
+
+
+
+        public void removeLastLine(RichTextBox rtb)
+        {
+            rtb.Lines = rtb.Lines.Take(rtb.Lines.Count() - 1).ToArray();
+        }
+
+        private void parseInitialInput()
+        {
+            foreach (string line in preParse.Lines)
+            {
+                if (line.Length == 10 && !line.StartsWith("//"))
+                {
+                    postParse.AppendText(line.Substring(2) + Environment.NewLine);
+                }
+            }
+            removeLastLine(postParse); //removes last line
+
+            List<string> textBoxStorage = new List<string>(postParse.Lines.ToList());
+            postParse.Clear();
+            foreach (string line in textBoxStorage)
+            {
+                string currentLine = Convert.ToString(Convert.ToInt32(line, 16), 2);
+
+                while (currentLine.Length < 32)
+                {
+                    currentLine = "0" + currentLine;
+                }
+
+                operations.Add(currentLine);
+
+                for (int i = 4; i < 35; i = i + 5)
+                {
+                    //currentLine = currentLine.Insert(i, " ");
+                }
+
+                string addme = "";
+
+                switch (currentLine.Substring(0, 2))
+                {
+                    case "00": postParse.AppendText("A/IF ");
+                        addme = currentLine.Substring(8,4) + " ";
+                        addme += currentLine.Substring(12, 4) + " ";
+                        addme += currentLine.Substring(16, 4) + " ";
+                        addme += currentLine.Substring(20, 12) + " ";
+                        break;
+                    case "01": postParse.AppendText("CBIF ");
+                        addme = currentLine.Substring(8, 4) + " ";
+                        addme += currentLine.Substring(12, 4) + " ";
+                        addme += currentLine.Substring(16, 16) + " ";
+                        break;
+                    case "10": postParse.AppendText("UNJF "); 
+                        addme = currentLine.Substring(8, 24) + " ";
+                        break;
+                    case "11": postParse.AppendText("I/OF ");
+                        addme = currentLine.Substring(8, 4) + " ";
+                        addme += currentLine.Substring(12, 4) + " ";
+                        addme += currentLine.Substring(16, 16) + " ";
+                        break;
+
+                }
+
+                switch (currentLine.Substring(2, 6))
+                {
+                    case "000000": postParse.AppendText("Read             "); break;
+                    case "000001": postParse.AppendText("Write            "); break;
+                    case "000010": postParse.AppendText("Store            "); break;
+                    case "000011": postParse.AppendText("Load             "); break;
+                    case "000100": postParse.AppendText("Move             "); break;
+                    case "000101": postParse.AppendText("Add              "); break;
+                    case "000110": postParse.AppendText("Subtract         "); break;
+                    case "000111": postParse.AppendText("Multiply         "); break;
+                    case "001000": postParse.AppendText("Divide           "); break;
+                    case "001001": postParse.AppendText("AND              "); break;
+                    case "001010": postParse.AppendText("OR               "); break;
+                    case "001011": postParse.AppendText("Move I           "); break;
+                    case "001100": postParse.AppendText("Add I            "); break;
+                    case "001101": postParse.AppendText("Multiply I       "); break;
+                    case "001110": postParse.AppendText("Divide I         "); break;
+                    case "001111": postParse.AppendText("Load I           "); break;
+                    case "010000": postParse.AppendText("Set Less Than    "); break;
+                    case "010001": postParse.AppendText("Set Less Than I  "); break;
+                    case "010010": postParse.AppendText("Halt             "); break;
+                    case "010011": postParse.AppendText("Next Instruction "); break;
+                    case "010100": postParse.AppendText("Jump             "); break;
+                    case "010101": postParse.AppendText("BEQ              "); break;
+                    case "010110": postParse.AppendText("BNE              "); break;
+                    case "010111": postParse.AppendText("BEZ              "); break;
+                    case "011000": postParse.AppendText("BNZ              "); break;
+                    case "011001": postParse.AppendText("BGZ              "); break;
+                    case "011010": postParse.AppendText("BLZ              "); break;
+
+                }
+                postParse.AppendText(addme + Environment.NewLine);
+                //postParse.AppendText(currentLine + " ");
+                //postParse.AppendText(Environment.NewLine);
+                //postParse.AppendText(currentLine + Environment.NewLine);
+            }
+        }
+
+        private void arithInstruction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
