2bfb9d4d8df87266f78ec0accb2f56d75cfe7153
Clinton Flowers <clintonflowers222@gmail.com>
2016-03-11 12:16:54 -0500

    Added grid container WIP

--- Program File Interpreter/InterpreterForm.Designer.cs
+++ Program File Interpreter/InterpreterForm.Designer.cs
@@ -34,10 +34,13 @@
             this.postParse = new System.Windows.Forms.RichTextBox();
             this.interpretButton = new System.Windows.Forms.Button();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // preParse
@@ -46,14 +49,14 @@
             this.preParse.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.preParse.Location = new System.Drawing.Point(0, 0);
             this.preParse.Name = "preParse";
-            this.preParse.Size = new System.Drawing.Size(760, 237);
+            this.preParse.Size = new System.Drawing.Size(760, 268);
             this.preParse.TabIndex = 0;
             this.preParse.Text = resources.GetString("preParse.Text");
             // 
             // loadTextFile
             // 
             this.loadTextFile.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.loadTextFile.Location = new System.Drawing.Point(0, 237);
+            this.loadTextFile.Location = new System.Drawing.Point(0, 268);
             this.loadTextFile.Margin = new System.Windows.Forms.Padding(0, 0, 3, 3);
             this.loadTextFile.Name = "loadTextFile";
             this.loadTextFile.Size = new System.Drawing.Size(760, 30);
@@ -65,17 +68,18 @@
             // postParse
             // 
             this.postParse.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.postParse.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.postParse.Location = new System.Drawing.Point(0, 0);
+            this.postParse.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postParse.Location = new System.Drawing.Point(3, 27);
+            this.postParse.MinimumSize = new System.Drawing.Size(473, 0);
             this.postParse.Name = "postParse";
-            this.postParse.Size = new System.Drawing.Size(760, 236);
+            this.postParse.Size = new System.Drawing.Size(473, 175);
             this.postParse.TabIndex = 2;
             this.postParse.Text = "";
             // 
             // interpretButton
             // 
             this.interpretButton.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.interpretButton.Location = new System.Drawing.Point(0, 267);
+            this.interpretButton.Location = new System.Drawing.Point(0, 298);
             this.interpretButton.Margin = new System.Windows.Forms.Padding(0, 0, 3, 3);
             this.interpretButton.Name = "interpretButton";
             this.interpretButton.Size = new System.Drawing.Size(760, 30);
@@ -101,10 +105,38 @@
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.postParse);
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
             this.splitContainer1.Size = new System.Drawing.Size(760, 537);
-            this.splitContainer1.SplitterDistance = 297;
+            this.splitContainer1.SplitterDistance = 328;
             this.splitContainer1.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.postParse, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.19512F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 87.80488F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(760, 205);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.Location = new System.Drawing.Point(5, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 0, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Instructions";
             // 
             // InterpreterForm
             // 
@@ -124,6 +156,7 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -136,6 +169,8 @@
         private System.Windows.Forms.RichTextBox postParse;
         private System.Windows.Forms.Button interpretButton;
         private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
